6dd8e20e857a8c2f89f2f434fd6a3902e2d14375
扫码改需求 <1043009478@qq.com>
2020-01-15 13:24:13 +0800

    部署脚本

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/hexo-blog/
+docker-compose down
+echo "关闭容器"
+
+docker rmi -f $(docker images |  grep "hexo-nginx"  | awk '{print $1}')
+echo "删除容器"
+
+git pull
+rm -rf public/
+yarn run build
+mv favicon.png public/
+echo "拉取代码，重新打包"
+
+docker-compose up -d
+echo "生成镜像，启动容器"
+
+docker ps
