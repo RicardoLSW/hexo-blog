d36f70496413b0e42e0985811f21cfeea688ecd7
扫码改需求 <1043009478@qq.com>
2020-01-15 13:35:44 +0800

    .

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
+echo "关闭容器"
 cd /home/hexo-blog/
 docker-compose down
-echo "关闭容器"
 
-docker rmi -f $(docker images |  grep "hexo-nginx"  | awk '{print $1}')
 echo "删除容器"
+docker rmi -f $(docker images |  grep "hexo-nginx"  | awk '{print $1}')
 
-git pull
 echo "拉取代码"
+git pull
 
-yarn install
 echo "安装依赖"
+yarn install
 
-yarn run build
 echo "打包"
+yarn run build
 
-cp favicon.png public/
 echo "复制网站图标到public"
+cp favicon.png public/
 
-docker-compose up -d
 echo "生成镜像，启动容器"
+docker-compose up -d
 
 docker ps
